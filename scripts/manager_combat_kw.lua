519a78534b915d6bb4789c8e82cad45a610ba783
George Taray <gtaray@hotmail.com>
2022-12-03 16:48:53 -0800

    Removed extra debug

--- scripts/manager_combat_kw.lua
+++ scripts/manager_combat_kw.lua
@@ -556,7 +556,6 @@ function activateUnit(nodeNext, bCommanderIsActive)
 end
 
 function parseUnitTrait(nodeTrait)
-	Debug.chat(nodeTrait)
 	local sDisplay = DB.getValue(nodeTrait, "name", "");
 	local aDisplayOptions = {};
 
