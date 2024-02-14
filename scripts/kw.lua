cf8c2980836ccccbb2ae4206bfb8c7ba47c46c97
MeAndUnique <shane.tim.parker@gmail.com>
2024-02-14 15:11:03 -0500

    Added dedicated PartyManagerKw.

--- scripts/kw.lua
+++ scripts/kw.lua
@@ -240,11 +240,6 @@ function onInit()
 
 	fActionRoll = ActionsManager.actionRoll;
 	ActionsManager.actionRoll = actionRoll;
-
-	WindowTabManager.registerTab("partysheet_host", { sName = "domain", sIcon = "tab_domain", sClass = "ps_domain" });
-	WindowTabManager.registerTab("partysheet_host", { sName = "powerpool", sIcon = "tab_powerpool", sClass = "ps_powerpool" });
-	WindowTabManager.registerTab("partysheet_client", { sName = "domain", sIcon = "tab_domain", sClass = "ps_domain" });
-	WindowTabManager.registerTab("partysheet_client", { sName = "powerpool", sIcon = "tab_powerpool", sClass = "ps_powerpool" });
 end
 
 
